457bf83dbdcb95de80f612e7e821593860e7c5cd
Hans <hansie211@hotmail.com>
2020-03-31 15:57:22 +0200

    Prefix Menu selected item

--- ORKK/MainWindow.xaml.cs
+++ ORKK/MainWindow.xaml.cs
@@ -10,96 +10,116 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
 
-namespace ORKK
-{
+namespace ORKK {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window, INotifyPropertyChanged
-    {
+    public partial class MainWindow : Window, INotifyPropertyChanged {
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected void OnPropertyChanged(string propertyName = null)
-        {
-            if (PropertyChanged == null)
-            {
+        protected void OnPropertyChanged( string propertyName = null ) {
+            if ( PropertyChanged == null ) {
                 return;
             }
 
-            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            PropertyChanged.Invoke( this, new PropertyChangedEventArgs( propertyName ) );
         }
 
-        public ObservableCollection<CableChecklistObject> Checklists
-        {
-            get
-            {
-                if (ActiveOrder == null)
-                {
+        public ObservableCollection<CableChecklistObject> Checklists {
+            get {
+                if ( ActiveOrder == null ) {
                     return null;
                 }
 
-                return DataVault.GetChildCableChecklists(ActiveOrder.ID);
+                return DataVault.GetChildCableChecklists( ActiveOrder.ID );
+            }
+        }
+
+        public sealed class MenuOrderItem {
+
+            public OrderObject Order { get; }
+            public bool IsSelected {
+
+                get {
+
+                    Console.WriteLine( $"Update for { Order }" );
+
+                    MainWindow mw = (MainWindow)App.Current.MainWindow;
+
+                    if ( mw.ActiveOrder == null ) {
+                        return false;
+                    }
+
+                    return Order.ID == mw.ActiveOrder.ID;
+                }
+                set {
+                    return;
+                }
+            }
+
+            public MenuOrderItem( OrderObject order ) {
+
+                this.Order = order;
+            }
+
+            public override string ToString() {
+                return $"Order { Order.ID }";
             }
         }
 
         private CableChecklistObject _ActiveCableChecklist = null;
 
-        public CableChecklistObject ActiveCableChecklist
-        {
+        public CableChecklistObject ActiveCableChecklist {
             get => _ActiveCableChecklist;
-            set
-            {
+            set {
                 _ActiveCableChecklist = value;
-                OnPropertyChanged("ActiveChecklistItem");
+                OnPropertyChanged( "ActiveChecklistItem" );
             }
         }
 
-        public ObservableCollection<OrderObject> OrderList
-        {
+        public ObservableCollection<OrderObject> OrderList {
             get => OrderVault.GetOrders();
         }
+        public ICollection<MenuOrderItem> MenuOrderList { get; set; } = new ObservableCollection<MenuOrderItem>();
 
         private OrderObject _ActiveOrder = null;
 
-        public OrderObject ActiveOrder
-        {
+        public OrderObject ActiveOrder {
             get => _ActiveOrder;
-            set
-            {
+            set {
                 _ActiveOrder = value;
-                OnPropertyChanged("ActiveOrder");
-                OnPropertyChanged("Checklists");
+                OnPropertyChanged( "ActiveOrder" );
+                OnPropertyChanged( "Checklists" );
             }
         }
 
-        public IList<Damage> DamageTypes
-        {
-            get
-            {
-                return Enum.GetValues(typeof(Damage)).Cast<Damage>().ToArray();
+        public IList<Damage> DamageTypes {
+            get {
+                return Enum.GetValues( typeof( Damage ) ).Cast<Damage>().ToArray();
             }
         }
 
-        public bool AnyOrders
-        {
-            get
-            {
+        public bool AnyOrders {
+            get {
                 return OrderList.Any();
             }
         }
 
-        public MainWindow()
-        {
+        public MainWindow() {
+
+            foreach ( OrderObject order in OrderList ) {
+
+                MenuOrderList.Add( new MenuOrderItem( order ) );
+            }
+
             DataContext = this;
             InitializeComponent();
         }
 
-        private bool SaveOrder()
-        {
+        private bool SaveOrder() {
 
-            if (ActiveOrder == null)
-            {
+            if ( ActiveOrder == null ) {
                 return true;
             }
 
@@ -109,20 +129,16 @@ namespace ORKK
             return false;
         }
 
-        private void SelectNewOrder(OrderObject orderObject)
-        {
+        private void SelectNewOrder( OrderObject orderObject ) {
 
-            if (orderObject.ID == ActiveOrder?.ID)
-            {
+            if ( orderObject.ID == ActiveOrder?.ID ) {
 
                 return;
             }
 
-            if (!SaveOrder())
-            {
+            if ( !SaveOrder() ) {
 
-                switch (MessageBox.Show("Fouten in order, kan niet opslaan. Wijzigingen verwerpen?", "Let op!", MessageBoxButton.YesNo))
-                {
+                switch ( MessageBox.Show( "Fouten in order, kan niet opslaan. Wijzigingen verwerpen?", "Let op!", MessageBoxButton.YesNo ) ) {
                     case MessageBoxResult.Yes:
                         break;
                     case MessageBoxResult.None:
@@ -133,74 +149,86 @@ namespace ORKK
 
             ActiveOrder = orderObject;
 
-            OnPropertyChanged("OrderList");
+            OnPropertyChanged( "OrderList" );
         }
 
-        private void CloseWindow_Click(object sender, RoutedEventArgs e)
-        {
+        private void CloseWindow_Click( object sender, RoutedEventArgs e ) {
 
             Close();
         }
 
-        private void NewChecklist_Click(object sender, RoutedEventArgs e)
-        {
-            if (ActiveOrder is null)
-            {
+        private void NewChecklist_Click( object sender, RoutedEventArgs e ) {
+            if ( ActiveOrder is null ) {
                 return;
             }
 
             CableChecklistObject checklist = new CableChecklistObject(cableID, ActiveOrder.ID, 0, 0, 0, 0, 0, 0, 0, 0);
             cableID++;
-            CableChecklistVault.AddCableChecklist(checklist);
-            OnPropertyChanged("Checklists");
+            CableChecklistVault.AddCableChecklist( checklist );
+            OnPropertyChanged( "Checklists" );
         }
 
-        private void DeleteChecklist_Click(object sender, RoutedEventArgs e)
-        {
-            if (ActiveCableChecklist is null)
-            {
+        private void DeleteChecklist_Click( object sender, RoutedEventArgs e ) {
+            if ( ActiveCableChecklist is null ) {
                 return;
             }
 
-            CableChecklistVault.RemoveCableChecklist(ActiveCableChecklist.ID);
-            OnPropertyChanged("Checklists");
+            CableChecklistVault.RemoveCableChecklist( ActiveCableChecklist.ID );
+            OnPropertyChanged( "Checklists" );
         }
 
         int orderID = 15;
         int cableID = 10;
 
-        private void NewOrder_Click(object sender, RoutedEventArgs e)
-        {
+        private void NewOrder_Click( object sender, RoutedEventArgs e ) {
             OrderObject order = new OrderObject(orderID, string.Empty, DateTime.Now, string.Empty, string.Empty, null, 0, string.Empty);
             orderID++;
-            OrderVault.AddOrder(order);
+            OrderVault.AddOrder( order );
             ActiveOrder = order;
-            OnPropertyChanged("OrderList");
-            OnPropertyChanged("AnyOrders");
+
+            MenuOrderList.Add( new MenuOrderItem( order ) );
+
+            OnPropertyChanged( "OrderList" );
+            OnPropertyChanged( "AnyOrders" );
+            OnPropertyChanged( "MenuOrderList" );
+
         }
 
-        private void SelectOrder_Click(object sender, RoutedEventArgs e)
-        {
-            OrderObject order = (OrderObject)((MenuItem)sender).Header;
-            ActiveOrder = order;
+        private void SelectOrder_Click( object sender, RoutedEventArgs e ) {
+
+            MenuItem menuItem           = (MenuItem)sender;
+            MenuOrderItem menuOrderItem = (MenuOrderItem)menuItem.Header;
+
+            OrderObject order   = menuOrderItem.Order;
+            ActiveOrder         = order;
+
+            OnPropertyChanged( "MenuOrderList" );
         }
 
-        private void DeleteOrder_Click(object sender, RoutedEventArgs e)
-        {
+        private void DeleteOrder_Click( object sender, RoutedEventArgs e ) {
             OrderObject order = ActiveOrder;
             ActiveOrder = null;
-            OrderVault.RemoveOrder(order.ID);
-            OnPropertyChanged("OrderList");
-            OnPropertyChanged("AnyOrders");
+            OrderVault.RemoveOrder( order.ID );
+
+            foreach ( MenuOrderItem item in MenuOrderList ) {
+
+                if ( item.Order.ID == order.ID ) {
+
+                    MenuOrderList.Remove( item );
+                    break;
+                }
+            }
+
+            OnPropertyChanged( "OrderList" );
+            OnPropertyChanged( "AnyOrders" );
+            OnPropertyChanged( "MenuOrderList" );
         }
 
-        private void Save_Click(object sender, RoutedEventArgs e)
-        {
+        private void Save_Click( object sender, RoutedEventArgs e ) {
 
         }
 
-        private void Canvas_ManipulationStarted(object sender, ManipulationStartedEventArgs e)
-        {
+        private void Canvas_ManipulationStarted( object sender, ManipulationStartedEventArgs e ) {
             Canvas canvas = (Canvas)sender;
 
             Ellipse el = new Ellipse
@@ -210,9 +238,21 @@ namespace ORKK
                 Fill = new SolidColorBrush(Colors.Red)
             };
 
-            Canvas.SetLeft(el, e.ManipulationOrigin.X);
-            Canvas.SetTop(el, e.ManipulationOrigin.Y);
-            canvas.Children.Add(el);
+            Canvas.SetLeft( el, e.ManipulationOrigin.X );
+            Canvas.SetTop( el, e.ManipulationOrigin.Y );
+            canvas.Children.Add( el );
+        }
+
+        private void MainMenu_SubmenuOpened( object sender, RoutedEventArgs e ) {
+
+            MenuItem menu = (MenuItem)sender;
+
+            foreach ( MenuItem sub in menu.Items ) {
+
+                sub.GetBindingExpression( MenuItem.IsCheckedProperty ).UpdateTarget();
+            }
+
+            //( (MenuItem)sender ).GetBindingExpression( MenuItem.IsCheckedProperty ).UpdateTarget();
         }
     }
 }
